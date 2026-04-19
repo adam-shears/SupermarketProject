1d0fe3d1458e8b01bdfbac7ba5116966a1baebe0
Adam Shears <93818679+adam-shears@users.noreply.github.com>
2026-04-19 16:44:29 +0100

    chore: add initial files for recommendation system

---
+++ services/analytics/src/recommendations.js
@@ -0,0 +1,13 @@
+/* This file is responsible for implementing algorithmic recommendations.
+
+All functions are called from service.js and should be pure functions that take in data and return transformed data.
+
+Responsibilities:
+- Implementing recommendation algorithms
+- Transforming data for recommendations
+
+This file should not be responsible for:
+- Making SQL queries (db.js)
+- Handling HTTP requests and responses (routes.js)
+- Any logic that is not directly related to the recommendation algorithms
+*/
